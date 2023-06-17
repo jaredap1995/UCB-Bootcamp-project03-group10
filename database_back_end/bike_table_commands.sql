c7fa49678bacddb26d9a515c50e18f195089228d
jaredap1995 <jaredaperez1995@gmail.com>
2023-06-17 13:51:34 -0700

    .

--- database_back_end/bike_table_commands.sql
+++ database_back_end/bike_table_commands.sql
@@ -1,7 +1,16 @@
 CREATE TABLE bikes (id serial primary key,
-Date date,
+Date timestamp,
 total int,
 east int,
 west int);
+
+CREATE TABLE Burke_Gilman (id serial primary key,
+Date timestamp,
+total_ped_and_bike int,
+pedestrian_south int,
+pedestrian_north int,
+bike_north int,
+bike_south int);
+
 
 \copy bikes (Date, total, east, west) from '/Users/jaredperez/Documents/UCB-VIRT-DATA-PT-02-2023-U-LOLC/UCB-Bootcamp-project03-group10/database_back_end/Fremont_Bike.csv' WITH (format csv, HEADER true);
--- database_back_end/flask_app.py
+++ database_back_end/flask_app.py
@@ -10,11 +10,7 @@ import numpy as np
 # Database Setup
 #################################################
 
-conn = psycopg2.connect(host="chunee.db.elephantsql.com", 
-                        port = 5432, 
-                        database="hgykzxtn", 
-                        user="hgykzxtn", 
-                        password="0-A1hdKOOMmFxwFsFmO3JNByMhhRBew-")
+conn = psycopg2.connect()
 
 cursor=conn.cursor()
 cursor.execute("SELECT * FROM bikes")
