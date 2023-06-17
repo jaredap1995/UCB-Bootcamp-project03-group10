6acc7d936cf7166e51f9fb5be58833271ffd82e7
jaredap1995 <jaredaperez1995@gmail.com>
2023-06-17 13:02:04 -0700

    Initial Connection Script

--- database_back_end/bike_table_commands.sql
+++ database_back_end/bike_table_commands.sql
@@ -4,6 +4,4 @@ total int,
 east int,
 west int);
 
-\copy bikes (Date, total, east, west) from 
-'/Users/jaredperez/Documents/UCB-VIRT-DATA-PT-02-2023-U-LOLC/UCB-Bootcamp-project03-group10/database_back_end/Fremont_Bike.csv' WITH (format csv, 
-HEADER true);
+\copy bikes (Date, total, east, west) from '/Users/jaredperez/Documents/UCB-VIRT-DATA-PT-02-2023-U-LOLC/UCB-Bootcamp-project03-group10/database_back_end/Fremont_Bike.csv' WITH (format csv, HEADER true);
---
+++ database_back_end/flask_app.py
@@ -0,0 +1,29 @@
+from flask import Flask, jsonify
+import psycopg2
+import pandas as pd
+import numpy as np
+
+
+
+
+#################################################
+# Database Setup
+#################################################
+
+conn = psycopg2.connect(host="chunee.db.elephantsql.com", 
+                        port = 5432, 
+                        database="hgykzxtn", 
+                        user="hgykzxtn", 
+                        password="0-A1hdKOOMmFxwFsFmO3JNByMhhRBew-")
+
+cursor=conn.cursor()
+cursor.execute("SELECT * FROM bikes")
+if cursor.fetchall():
+    print("Connection successful")
+else:
+    print("Connection unsuccessful")
+
+conn.close()
+
+#################################################
+
