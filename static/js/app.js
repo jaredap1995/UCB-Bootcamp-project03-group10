b7784ac2dfa66a76e505f1907fb7628dbcb0c1ef
Wheezyotter <jacobpohs@gmail.com>
2023-06-20 13:12:14 -0700

    bugtesting

--- static/js/app.js
+++ static/js/app.js
@@ -2,6 +2,7 @@
 //the second d3.json(url).then() block is chained to the first one using the return statement. 
 //This ensures that the second API call is made only after the first one is completed successfully. 
 //By chaining the promises, the code will fetch data for both charts and plot them accordingly.
+// New edit for a test
 
 let url_2 = "/api/broadway";
 let url = "/api/alldata";
