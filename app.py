206ef0cbad0f8e673aaffe36ab5b8459647c8aca
Wheezyotter <jacobpohs@gmail.com>
2023-06-27 16:54:01 -0700

    renamed for merging

--- app.py
+++ app.py
@@ -186,5 +186,47 @@ def fremont_hourly():
     return jsonify(bikes_list)
 
 
+@app.route("/api/broadway_year")
+def broadway_year():
+    query = '''Select date_part('year', date) as year, SUM(north_south_sum_broadway), SUM(north_bike_broadway), SUM(south_bike_broadway)
+    from broadway
+    group by year'''
+    cursor.execute(query)
+
+    first = cursor.fetchall()
+
+    bikes_list = []
+    for date, total, north_bike, south_bike in first:
+        dict_bikes = {}
+        dict_bikes['year'] = date
+        dict_bikes['total'] = total
+        dict_bikes['north_bike'] = north_bike
+        dict_bikes['south_bike'] = south_bike
+        bikes_list.append(dict_bikes)
+
+    return jsonify(bikes_list)
+
+@app.route("/api/burkegilman_year")
+def burke_gilman_year():
+    query = '''Select date_part('year', date) as year, SUM(total_ped_and_bike_burke), SUM(pedestrian_south), SUM(pedestrian_north_burke), SUM(bike_north_burke), SUM(bike_south_burke)
+    from burke_gilman
+    group by year'''
+    cursor.execute(query)
+
+    first = cursor.fetchall()
+
+    bikes_ped_list = []
+    for date, total_ped_and_bike, pedestrian_south, pedestrian_north,bike_north, bike_south in first:
+        dict_bikes = {}
+        dict_bikes['year'] = date
+        dict_bikes['total_ped_and_bike'] = total_ped_and_bike
+        dict_bikes['pedestrian_south'] = pedestrian_south
+        dict_bikes['pedestrian_north'] = pedestrian_north
+        dict_bikes['bike_north'] = bike_north
+        dict_bikes['bike_south'] = bike_south
+        bikes_ped_list.append(dict_bikes)
+
+    return jsonify(bikes_ped_list)
+
 if __name__ == '__main__':
     app.run(port=8000, debug=True)
---
+++ static/js/jake_app.js
@@ -0,0 +1,788 @@
+window.Apex = {
+  dataLabels: {
+    enabled: false
+  }
+};
+
+var chartBarLeft = []
+var optionsBarLeft = []
+var optionsBarRight = []
+var chartBarRight = []
+
+var totalLayout = []
+var totalChartData = []
+var trace1 = []
+
+function init(){
+  optionsBarLeft = {
+    chart: {
+      type: 'bar',
+      height: 250,
+      width: '100%',
+      stacked: true,
+      foreColor: '#000000',
+    },
+    dataLabels: {
+        enabled: false
+    },
+    series: [],
+    title: {
+        text: 'Bike Traffic',
+    },
+    noData: {
+      text: 'Choose a Location...'
+    }
+  }
+  chartBarLeft = new ApexCharts(document.querySelector("#bar"), optionsBarLeft);
+  chartBarLeft.render();
+
+  optionsBarRight = {
+    chart: {
+      type: 'bar',
+      height: 250,
+      width: '100%',
+      stacked: true,
+      foreColor: '#000000',
+    },
+    dataLabels: {
+        enabled: false
+    },
+    series: [],
+    title: {
+        text: 'Bike Traffic',
+    },
+    noData: {
+      text: 'Choose a Location...'
+    }
+  }
+  chartBarRight = new ApexCharts(document.querySelector("#barb"), optionsBarRight);
+  chartBarRight.render();
+
+  totalLayout = {
+    title: 'Total Bike Traffic',
+    xaxis: {
+      autorange: false,
+      range: ['2014', '2021'],
+      rangeselector: {buttons: [
+          {
+            count: 1,
+            label: '1d',
+            step: 'day',
+            stepmode: 'backward'
+          },
+          {
+            count: 1,
+            label: '1w',
+            step: 'week',
+            stepmode: 'backward'
+          },
+          {
+            count: 1,
+            label: '1m',
+            step: 'month',
+            stepmode: 'backward'
+          },
+          {
+            count: 6,
+            label: '6m',
+            step: 'month',
+            stepmode: 'backward'
+          },
+          {
+            count: 1,
+            label: '1y',
+            step: 'year',
+            stepmode: 'backward'
+          },
+          {step: 'all'}
+      ]},
+      rangeslider: {range: ['2014', '2021']},
+      type: 'date'
+    },
+    yaxis: {
+      autorange: true,
+      range: [0, 1000],
+      type: 'linear'
+    }
+  }
+  totalChartData = [{}]
+  Plotly.newPlot("barbg", totalChartData, totalLayout);
+};
+
+d3.selectAll('#selLocation').on("change", updateVisuals);
+
+function updateVisuals() {
+  // Use D3 to select the dropdown menu
+  let dropdownMenuLocation = d3.select("#selLocation");
+  // Assign the value of the dropdown menu option to a variable
+  let location = dropdownMenuLocation.property("value");
+
+  let jsonUrl = [];
+
+  let dataDates = [];
+  let dataDirectionOne = [];
+  let dataDirectionTwo = [];
+  let dataTotal = [];
+
+  let leftYData = [];
+  let leftXData = [];
+  let rightYData = [];
+  let rightXData = [];
+
+  if (location == 'Broadway') {
+    // Broadway
+    jsonUrl = "/api/broadway_year";
+    d3.json(jsonUrl)
+      .then(function(data) {
+        data.forEach((elem) => {
+          dataDates.push(elem.year);
+          dataDirectionOne.push(elem.north_bike);
+          dataDirectionTwo.push(elem.south_bike);
+          dataTotal.push(elem.total);
+        });
+        leftYData = dataDirectionOne;
+        leftXData = dataDates;
+        rightYData = dataDirectionTwo;
+        rightXData = dataDates;
+
+        chartBarLeft.updateSeries([{
+          data: leftYData
+        }]);
+        chartBarLeft.updateOptions({
+          chart: {
+                type: 'bar',
+                height: 250,
+                width: '100%',
+                stacked: true,
+                foreColor: '#999',
+          },
+          animations:{
+            enabled: true,
+            easing: 'easeinout',
+            speed: 800,
+            animateGradually: {
+                enabled: true,
+                delay: 150
+            },
+            dynamicAnimation: {
+                enabled: true,
+                speed: 350
+            }
+          },
+          plotOptions: {
+            bar: {
+              dataLabels: {
+                enabled: false
+              },
+              columnWidth: '75%',
+              endingShape: 'rounded'
+            }
+          },
+          colors: ["#00C5A4"], //add additional colors here for multiple dataset ex:[color1,color2]
+          series: [{
+            name: "Bikes",
+            data: leftYData,
+          }, 
+          ],
+          labels: leftXData,
+          xaxis: {
+            axisBorder: {
+              show: false
+            },
+            axisTicks: {
+              show: true
+            },
+            crosshairs: {
+              show: false
+            },
+            labels: {
+              show: true,
+              style: {
+                fontSize: '14px'
+              }
+            },
+          },
+          grid: {
+            xaxis: {
+              lines: {
+                show: false
+              },
+            },
+            yaxis: {
+              lines: {
+                show: true
+              },
+            }
+          },
+          yaxis: {
+            axisBorder: {
+              show: false
+            },
+            labels: {
+              show: true
+            },
+          },
+          legend: {
+            floating: true,
+            position: 'top',
+            horizontalAlign: 'right',
+            offsetY: -36
+          },
+          title: {
+            text: 'Northbound',
+            align: 'left',
+          },
+          tooltip: {
+            shared: true,
+            intersect: false
+          }
+        });
+
+        chartBarRight.updateSeries([{
+          data: rightYData
+        }]);
+        chartBarRight.updateOptions({
+          chart: {
+              type: 'bar',
+              height: 250,
+              width: '100%',
+              stacked: true,
+              foreColor: '#999',
+            },
+            animations:{
+              enabled: true,
+              easing: 'easeinout',
+              speed: 800,
+              animateGradually: {
+                  enabled: true,
+                  delay: 150
+              },
+              dynamicAnimation: {
+                  enabled: true,
+                  speed: 350
+              }
+            },
+            plotOptions: {
+              bar: {
+                dataLabels: {
+                  enabled: false
+                },
+                columnWidth: '75%',
+                endingShape: 'rounded'
+              }
+            },
+            colors: ["#f4516c"], //add additional colors here for multiple dataset ex:[color1,color2]
+            series: [{
+              name: "Bikes",
+              data: rightYData,
+            }, 
+            // {
+            //   name: "dataset2",
+            //   data: [20, 16, 24, 28, 26, 22, 15, 5, 14, 16, 22, 29, 24, 19, 15, 10, 11, 15, 19, 23],
+            // }
+            ],
+            labels: rightXData,
+            xaxis: {
+              axisBorder: {
+                show: false
+              },
+              axisTicks: {
+                show: true
+              },
+              crosshairs: {
+                show: false
+              },
+              labels: {
+                show: true,
+                style: {
+                  fontSize: '14px'
+                }
+              },
+            },
+            grid: {
+              xaxis: {
+                lines: {
+                  show: false
+                },
+              },
+              yaxis: {
+                lines: {
+                  show: true
+                },
+              }
+            },
+            yaxis: {
+              axisBorder: {
+                show: false
+              },
+              labels: {
+                show: true
+              },
+            },
+            legend: {
+              floating: true,
+              position: 'top',
+              horizontalAlign: 'right',
+              offsetY: -36
+            },
+            title: {
+              text: 'South Bound',
+              align: 'left',
+            },
+            tooltip: {
+              shared: false,
+              intersect: false
+            }
+        });
+
+        trace1 = {
+          type: "scatter",
+          mode: "lines",
+          name: 'Total Bike Traffic',
+          x: leftXData,
+          y: dataTotal,
+          line: {color: '#30b842'}
+        }
+      
+        totalChartData = [trace1];
+        Plotly.newPlot("barbg", totalChartData, totalLayout);
+
+      });
+  }
+
+  else if (location == 'Fremont') {
+    // Fremont
+    jsonUrl = "/api/fremont_year";
+    d3.json(jsonUrl)
+      .then(function(data) {
+        data.forEach((elem) => {
+          dataDates.push(elem.year);
+          dataDirectionOne.push(elem.east);
+          dataDirectionTwo.push(elem.west);
+          dataTotal.push(elem.total);
+        });
+      leftYData = dataDirectionOne;
+      leftXData = dataDates;
+      rightYData = dataDirectionTwo;
+      rightXData = dataDates;
+
+      chartBarLeft.updateSeries([{
+        data: leftYData
+      }]);
+      chartBarLeft.updateOptions({
+        chart: {
+          type: 'bar',
+          height: 250,
+          width: '100%',
+          stacked: true,
+          foreColor: '#999',
+        },
+        animations:{
+          enabled: true,
+          easing: 'easeinout',
+          speed: 800,
+          animateGradually: {
+              enabled: true,
+              delay: 150
+          },
+          dynamicAnimation: {
+              enabled: true,
+              speed: 350
+          }
+        },
+        plotOptions: {
+          bar: {
+            dataLabels: {
+              enabled: false
+            },
+            columnWidth: '75%',
+            endingShape: 'rounded'
+          }
+        },
+        colors: ["#00C5A4"], //add additional colors here for multiple dataset ex:[color1,color2]
+        series: [{
+          name: "Bikes",
+          data: leftYData,
+        }, 
+        ],
+        labels: leftXData,
+        xaxis: {
+          axisBorder: {
+            show: false
+          },
+          axisTicks: {
+            show: true
+          },
+          crosshairs: {
+            show: false
+          },
+          labels: {
+            show: true,
+            style: {
+              fontSize: '14px'
+            }
+          },
+        },
+        grid: {
+          xaxis: {
+            lines: {
+              show: false
+            },
+          },
+          yaxis: {
+            lines: {
+              show: true
+            },
+          }
+        },
+        yaxis: {
+          axisBorder: {
+            show: false
+          },
+          labels: {
+            show: true
+          },
+        },
+        legend: {
+          floating: true,
+          position: 'top',
+          horizontalAlign: 'right',
+          offsetY: -36
+        },
+        title: {
+          text: 'East Bound',
+          align: 'left',
+        },
+        tooltip: {
+          shared: true,
+          intersect: false
+        }
+      });
+
+      chartBarRight.updateSeries([{
+        data: rightYData
+      }]);
+      chartBarRight.updateOptions({
+        chart: {
+            type: 'bar',
+            height: 250,
+            width: '100%',
+            stacked: true,
+            foreColor: '#999',
+          },
+          animations:{
+            enabled: true,
+            easing: 'easeinout',
+            speed: 800,
+            animateGradually: {
+                enabled: true,
+                delay: 150
+            },
+            dynamicAnimation: {
+                enabled: true,
+                speed: 350
+            }
+          },
+          plotOptions: {
+            bar: {
+              dataLabels: {
+                enabled: false
+              },
+              columnWidth: '75%',
+              endingShape: 'rounded'
+            }
+          },
+          colors: ["#f4516c"], //add additional colors here for multiple dataset ex:[color1,color2]
+          series: [{
+            name: "Bikes",
+            data: rightYData,
+          }, 
+          ],
+          labels: rightXData,
+          xaxis: {
+            axisBorder: {
+              show: false
+            },
+            axisTicks: {
+              show: true
+            },
+            crosshairs: {
+              show: false
+            },
+            labels: {
+              show: true,
+              style: {
+                fontSize: '14px'
+              }
+            },
+          },
+          grid: {
+            xaxis: {
+              lines: {
+                show: false
+              },
+            },
+            yaxis: {
+              lines: {
+                show: true
+              },
+            }
+          },
+          yaxis: {
+            axisBorder: {
+              show: false
+            },
+            labels: {
+              show: true
+            },
+          },
+          legend: {
+            floating: true,
+            position: 'top',
+            horizontalAlign: 'right',
+            offsetY: -36
+          },
+          title: {
+            text: 'West Bound',
+            align: 'left',
+          },
+          tooltip: {
+            shared: false,
+            intersect: false
+          }
+      });
+      trace1 = {
+        type: "scatter",
+        mode: "lines",
+        name: 'Total Bike Traffic',
+        x: leftXData,
+        y: dataTotal,
+        line: {color: '#30b842'}
+      }
+    
+      totalChartData = [trace1];
+      Plotly.newPlot("barbg", totalChartData, totalLayout);
+    });
+  }
+
+  else if (location == 'Burke Gilman') {
+    // Burke Gilman
+    jsonUrl = "/api/burkegilman_year";
+    d3.json(jsonUrl)
+      .then(function(data) {
+        data.forEach((elem) => {
+          dataDates.push(elem.year);
+          dataDirectionOne.push(elem.bike_north);
+          dataDirectionTwo.push(elem.bike_south);
+          dataTotal.push(elem.total);
+        });
+      leftYData = dataDirectionOne;
+      leftXData = dataDates;
+      rightYData = dataDirectionTwo;
+      rightXData = dataDates ;
+
+      chartBarLeft.updateSeries([{
+        data: leftYData
+      }]);
+      chartBarLeft.updateOptions({
+        chart: {
+              type: 'bar',
+              height: 250,
+              width: '100%',
+              stacked: true,
+              foreColor: '#999',
+        },
+        animations:{
+          enabled: true,
+          easing: 'easeinout',
+          speed: 800,
+          animateGradually: {
+              enabled: true,
+              delay: 150
+          },
+          dynamicAnimation: {
+              enabled: true,
+              speed: 350
+          }
+        },
+        plotOptions: {
+          bar: {
+            dataLabels: {
+              enabled: false
+            },
+            columnWidth: '75%',
+            endingShape: 'rounded'
+          }
+        },
+        colors: ["#00C5A4"], //add additional colors here for multiple dataset ex:[color1,color2]
+        series: [{
+          name: "Bikes",
+          data: leftYData,
+        }, 
+        ],
+        labels: leftXData,
+        xaxis: {
+          axisBorder: {
+            show: false
+          },
+          axisTicks: {
+            show: true
+          },
+          crosshairs: {
+            show: false
+          },
+          labels: {
+            show: true,
+            style: {
+              fontSize: '14px'
+            }
+          },
+        },
+        grid: {
+          xaxis: {
+            lines: {
+              show: false
+            },
+          },
+          yaxis: {
+            lines: {
+              show: true
+            },
+          }
+        },
+        yaxis: {
+          axisBorder: {
+            show: false
+          },
+          labels: {
+            show: true
+          },
+        },
+        legend: {
+          floating: true,
+          position: 'top',
+          horizontalAlign: 'right',
+          offsetY: -36
+        },
+        title: {
+          text: 'North bound',
+          align: 'left',
+        },
+        tooltip: {
+          shared: true,
+          intersect: false
+        }
+      });
+
+      chartBarRight.updateSeries([{
+        data: rightYData
+      }]);
+      chartBarRight.updateOptions({
+        chart: {
+            type: 'bar',
+            height: 250,
+            width: '100%',
+            stacked: true,
+            foreColor: '#999',
+          },
+          animations:{
+            enabled: true,
+            easing: 'easeinout',
+            speed: 800,
+            animateGradually: {
+                enabled: true,
+                delay: 150
+            },
+            dynamicAnimation: {
+                enabled: true,
+                speed: 350
+            }
+          },
+          plotOptions: {
+            bar: {
+              dataLabels: {
+                enabled: false
+              },
+              columnWidth: '75%',
+              endingShape: 'rounded'
+            }
+          },
+          colors: ["#f4516c"], //add additional colors here for multiple dataset ex:[color1,color2]
+          series: [{
+            name: "Bikes",
+            data: rightYData,
+          }, 
+          // {
+          //   name: "dataset2",
+          //   data: [20, 16, 24, 28, 26, 22, 15, 5, 14, 16, 22, 29, 24, 19, 15, 10, 11, 15, 19, 23],
+          // }
+          ],
+          labels: rightXData,
+          xaxis: {
+            axisBorder: {
+              show: false
+            },
+            axisTicks: {
+              show: true
+            },
+            crosshairs: {
+              show: false
+            },
+            labels: {
+              show: true,
+              style: {
+                fontSize: '14px'
+              }
+            },
+          },
+          grid: {
+            xaxis: {
+              lines: {
+                show: false
+              },
+            },
+            yaxis: {
+              lines: {
+                show: true
+              },
+            }
+          },
+          yaxis: {
+            axisBorder: {
+              show: false
+            },
+            labels: {
+              show: true
+            },
+          },
+          legend: {
+            floating: true,
+            position: 'top',
+            horizontalAlign: 'right',
+            offsetY: -36
+          },
+          title: {
+            text: 'South Bound',
+            align: 'left',
+          },
+          tooltip: {
+            shared: false,
+            intersect: false
+          }
+      });
+      trace1 = {
+        type: "scatter",
+        mode: "lines",
+        name: 'Total Bike Traffic',
+        x: leftXData,
+        y: dataTotal,
+        line: {color: '#30b842'}
+      }
+    
+      totalChartData = [trace1];
+      Plotly.newPlot("barbg", totalChartData, totalLayout);
+    });
+  }
+};
+
+init();
